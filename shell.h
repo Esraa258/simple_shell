bc275ac6247376115f1a0efa30fe85c8ee838ccf
Esraa258 <esraa.tarek3690@gmail.com>
2023-08-22 21:25:00 -0700

    My first commit

--- shell.h
+++ shell.h
@@ -193,14 +193,14 @@ void clear_info(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
 
-/* toem_environ.c */
+/* getenviron.c */
 char *_getenv(info_t *, const char *);
 int _myenv(info_t *);
 int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 
-/* toem_getenv.c */
+/* getenviron2.c */
 char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
